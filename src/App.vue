f3fc33f6e8c286c9845ee579ca89079b4270de5b
Neageadon <legoog.tsop@gmail.com>
2019-06-10 20:31:13 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout  view="hhh lpR fFf" id="q-app">
+    <drawer></drawer>
+    <router-view></router-view>
+  </q-layout >
+</template>
+
+<script>
+import drawer from '../src/layouts/system/drawerToolbar'
+export default {
+  name: 'App',
+  components: {
+    drawer
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/layouts/system/drawerToolbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <q-header  reveal bordered class="bg-deep-purple-10 text-white">
+      <q-toolbar>
+        <q-btn  dense flat round icon="menu" @click="drawer = !drawer"></q-btn>
+
+        <q-toolbar-title>
+          <q-btn flat to="/AssociationsMethod"><q-icon  name="home"></q-icon></q-btn>
+          Boba app
+        </q-toolbar-title>
+        <q-btn v-if="!isUserAuthorized" class="" to="/Authentication">Login</q-btn>
+        <q-btn v-else class="" @click="logout" >Logout</q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="drawer" side="left" overlay elevated>
+      <!-- drawer content -->
+      <div class="row q-gutter-md">
+        <q-btn class="col-11" to="/AssociationsMethod">AM</q-btn>
+        <q-btn class="col-11" to="/EmotionsRead">RE</q-btn>
+      </div>
+    </q-drawer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  data () {
+    return {
+      drawer: true
+    }
+  },
+
+  methods: {
+    logout () {
+      this.$store.dispatch('logout')
+    }
+
+  },
+
+  computed: {
+    isUserAuthorized () {
+      return this.$store.getters.isUserAuthorized
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
